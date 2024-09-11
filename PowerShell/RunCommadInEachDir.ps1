c8e814a632fc51532a9493eb2c91644b6aaacf29
trishaldoshi <trishaldoshi@gmail.com>
2024-09-11 16:43:24 +0800

    run command in each directory

---
+++ PowerShell/RunCommadInEachDir.ps1
@@ -0,0 +1,9 @@
+﻿$original_path = Get-Location
+$target_path = "./ref-codes"
+$cmd_exec = "git pull"
+$folders = Get-ChildItem -path $base_path -Directory
+$folders | ForEach-Object { 
+                            Write-Host processing $_.Name in $target_path -ForegroundColor Yellow
+                            Set-Location $_.FullName ; 
+                            Invoke-Expression $cmd_exec}
+Set-Location $original_path
